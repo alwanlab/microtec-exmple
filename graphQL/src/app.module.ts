3f24c6e9f34731f1c9ff723f1b35f8dbcb46c8a5
Mohamed Farraj <alwanlab.com@gmail.com>
2019-12-31 09:17:50 +0200

    edit

--- graphQL/src/app.module.ts
+++ graphQL/src/app.module.ts
@@ -3,10 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './modules/users/users.module';
 import { GeneralModule } from './modules/general/general.module';
 import { typeOrmConfig } from './common/config/typeorm.config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      installSubscriptionHandlers: true,
+    }),
     UsersModule,
     GeneralModule
   ]
